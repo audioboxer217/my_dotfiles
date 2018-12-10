ffe90cbe9aa59d04663daa2f5f8475f296f00520
scott.eppler <scott.eppler@coda.global>
2018-12-10 15:05:54 -0600

    Update to use Homebrew for `tfenv` install

--- setup_home_mac.sh
+++ setup_home_mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-brew install vim tmux ansible
+brew install vim tmux ansible tfenv
 
 git submodule update --init --recursive
 
@@ -12,12 +12,8 @@ ln -s /Users/scott/dotfiles/gitconfig /Users/scott/.gitconfig
 ln -s /Users/scott/dotfiles/presidio.gitconfig /Users/scott/presidio.gitconfig
 ln -s /Users/scott/dotfiles/ssh /Users/scott/.ssh
 ln -s /Users/scott/dotfiles/technologent.gitconfig /Users/scott/technologent.gitconfig
-ln -s /Users/scott/dotfiles/tfenv /Users/scott/.tfenv
 ln -s /Users/scott/dotfiles/tmux/tmux /Users/scott/.tmux
 ln -s /Users/scott/dotfiles/tmux/tmux.conf /Users/scott/.tmux.conf
 ln -s /Users/scott/dotfiles/vim /Users/scott/.vim
 
-ln -s /Users/scott/.tfenv/bin/tfenv /usr/local/bin/tfenv
 tfenv install latest
-
-ln -s /Users/scott/.tfenv/bin/terraform /usr/local/bin/terraform
