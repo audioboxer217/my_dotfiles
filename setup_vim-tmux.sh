d69958a685783ba9164623fea6741e9291f13aba
Scott Eppler <scott@scotts-mbp.lan>
2019-02-27 13:25:48 -0600

    Create version for k8s exam

---
+++ setup_vim-tmux.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get install -y vim tmux
+git submodule update --init --recursive
+
+ln -snf $(pwd)/tmux/tmux $HOME/.tmux
+ln -sf $(pwd)/tmux/tmux.conf $HOME/.tmux.conf
+ln -snf $(pwd)/vim $HOME/.vim
