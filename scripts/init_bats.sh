34edf64ba82ef63ea96cb461e9f529e166d6343a
scott.eppler <scott.eppler@coda.global>
2019-04-29 14:31:04 -0500

    Add init-bats.sh script
    This script sets up Bats testing for a project.

---
+++ scripts/init_bats.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p test/libs
+
+git submodule add https://github.com/sstephenson/bats test/libs/bats
+git submodule add https://github.com/ztombol/bats-support test/libs/bats-support
+git submodule add https://github.com/ztombol/bats-assert test/libs/bats-assert
+
+echo "# Run this file to run all the tests, once
+./test/libs/bats/bin/bats test/*.bats" > run_tests.sh
+
+echo '# Run this file (with 'entr' installed) to watch all files and rerun tests on changes
+ls -d **/* | entr ./run_tests.sh' > dev.sh
+
+chmod +x run_tests.sh dev.sh
