c7394a5aaa65b09d7b2156e4d56f375e91cdbd76
scott.eppler <scott.eppler@coda.global>
2019-06-06 16:16:40 -0500

    Fix issue with "oh-my-zsh" install script
    Since I am downloading then running unattended I had to add exec perms on the file.

--- setup_home.sh
+++ setup_home.sh
@@ -66,10 +66,12 @@ fi
 # Install oh-my-zsh
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sudo -u $username wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O $HOME/oh-my-zsh_install.sh
+  chmod +x $HOME/oh-my-zsh_install.sh
   sudo -u $username $HOME/oh-my-zsh_install.sh --unattended
 fi
 
 git submodule update --init --recursive
+
 
 mv $HOME/.bashrc $HOME/.bashrc_old
 
