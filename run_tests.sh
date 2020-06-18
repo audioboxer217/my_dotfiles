92c94ea584803ddf88a2396383505f2dd5eb2953
scott.eppler <scott.eppler@coda.global>
2020-06-18 13:51:01 -0500

    Update 'run_tests.sh'
    Set DIR variable to hold full path to script

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,6 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
 # Run this file to run all the tests, once
-./test/libs/bats/bin/bats test/*.bats
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
+$DIR/test/libs/bats/bin/bats test/*.bats
