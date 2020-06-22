da601421fef682e9424444aadc6921eb4079d87b
scott.eppler <scott.eppler@coda.global>
2020-06-22 15:33:06 -0500

    Fix Linting issue with 'init_bats.sh'

--- scripts/init_bats.sh
+++ scripts/init_bats.sh
@@ -9,7 +9,7 @@ git submodule add https://github.com/ztombol/bats-assert test/libs/bats-assert
 echo "# Run this file to run all the tests, once
 ./test/libs/bats/bin/bats test/*.bats" > run_tests.sh
 
-echo '# Run this file (with 'entr' installed) to watch all files and rerun tests on changes
+echo '# Run this file (with `entr` installed) to watch all files and rerun tests on changes
 ls -d **/* | entr ./run_tests.sh' > dev.sh
 
 chmod +x run_tests.sh dev.sh
