3ef8598cd34cbe13dc9b81fb729611e046c2dbf8
scott.eppler <scott.eppler@coda.global>
2020-06-22 16:19:25 -0500

    Add $DIR to Test List in run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,4 @@ set -eo pipefail
 
 # Run this file to run all the tests, once
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
-"${DIR}"/test/libs/bats/bin/bats test/*.bats
+"${DIR}"/test/libs/bats/bin/bats "${DIR}"/test/*.bats
